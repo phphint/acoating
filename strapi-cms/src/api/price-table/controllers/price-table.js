12d3a3059ac6737485d8a033575aeafa10de71b5
phphint <124293060+phphint@users.noreply.github.com>
2024-05-03 01:47:52 -0400

    fix strapi

--- strapi-cms/src/api/price-table/controllers/price-table.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * price-table controller
- */
-
-const { createCoreController } = require('@strapi/strapi').factories;
-
-module.exports = createCoreController('api::price-table.price-table');
