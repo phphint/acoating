d3c801aeedf532fdf93c52d2d8c9192df62b130e
phphint <124293060+phphint@users.noreply.github.com>
2024-05-04 07:54:14 -0400

    Update middlewares.js

--- strapi-config/middlewares.js
+++ strapi-config/middlewares.js
@@ -13,6 +13,9 @@ module.exports = [
     config: {
       enabled: true,
       multipart: true,
+      formLimit: "256mb", // modify form body
+      jsonLimit: "256mb", // modify JSON body
+      textLimit: "256mb", // modify text body
       formidable: {
         maxFileSize: 100 * 1024 * 1024 // 100 MB
       }
